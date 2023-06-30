35fb31de973b74032f47c7c3f8cd5a1cb324f4da
abluehope <isaiah9431@gmail.com>
2023-06-30 12:34:35 +0900

    20230630

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+const MainSlide = new Swiper(".main_slide", {
+  loop: true,
+});
+
+const leftArrow = document.querySelector("#MainVisual .arrows .left");
+leftArrow.addEventListener("click", function () {
+  MainSlide.slidePrev();
+});
+
+const rightArrow = document.querySelector("#MainVisual .arrows .right");
+rightArrow.addEventListener("click", function () {
+  MainSlide.slideNext();
+});
+
+const proSlide = new Swiper(".pro_slide", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".sbar",
+    type: "progressbar",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const mobileBtn = document.querySelector(".mobile_btn");
+const GNB = document.querySelector("#gnb");
+const H1 = document.querySelector("h1");
+
+mobileBtn.addEventListener("click", function () {
+  GNB.classList.toggle("on");
+  H1.classList.toggle("on");
+});
+
+GNB.addEventListener("wheel", function (e) {
+  if (GNB.classList.contains("on")) e.preventDefault();
+});
